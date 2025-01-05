96793bf04dc9783db60b399e36862c0bf222a6c0
yuqiao <qiao_yuheng@163.com>
2025-01-05 07:35:32 -0800

    250105@yuqiao

--- src/Scheduler.cc
+++ src/Scheduler.cc
@@ -1,1 +1,10 @@
 #include "../include/Scheduler.h"
+
+using namespace netco;
+
+Scheduler* Scheduler::_pScheduler = nullptr;
+std::mutex Scheduler::_schMutex;
+
+Scheduler::Scheduler()
+    : _proSeletor(_pros)
+{ }
